9c2833ccba8a7502390ec49d00653d474b09c8c4
adarsh <adarsh.goyal@revfin.in>
2025-04-21 22:45:57 +0530

    solved

--- Software Code/website/client/src/components/home-component/HomePart_1.jsx
+++ Software Code/website/client/src/components/home-component/HomePart_1.jsx
@@ -8,7 +8,7 @@ import Tags from "./component/Tags.jsx";
 import Navbar from "../Navbar.jsx";
 const HomePart_1 = () => {
   return (
-    <div className="bg-[url(../../src/assets/home-page/drawing-room.svg)] w-100% bg-cover px-6 xxxs:px-12 lg:px-18 font-roboto pb-26 bg-no-repeat">
+    <div className="bg-[url(../../../../assets/home-page/drawing-room.svg)] w-100% bg-cover px-6 xxxs:px-12 lg:px-18 font-roboto pb-26 bg-no-repeat">
       <Navbar brandColor="bg-gradient-to-b from-[#FF9238] from-20% to-white bg-clip-text text-transparent"/>
       <div className="1600b:mx-20 2000b:mx-40">
         <div className="py-10 md:py-16 lg:py-24 flex justify-between">
