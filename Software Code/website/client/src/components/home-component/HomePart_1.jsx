91049004d81d21d346afb5b63991146187d200d2
adarsh <adarsh.goyal@revfin.in>
2025-04-21 23:27:06 +0530

    g

--- Software Code/website/client/src/components/home-component/HomePart_1.jsx
+++ Software Code/website/client/src/components/home-component/HomePart_1.jsx
@@ -6,9 +6,10 @@ import {
 } from "../../assets/home-page/index";
 import Tags from "./component/Tags.jsx";
 import Navbar from "../Navbar.jsx";
+import homeImage from "../../../src/assets/home-page/drawing-room.svg"
 const HomePart_1 = () => {
   return (
-    <div className="bg-[url(../assets/home-page/drawing-room.svg)] w-100% bg-cover px-6 xxxs:px-12 lg:px-18 font-roboto pb-26 bg-no-repeat">
+    <div className="w-100% bg-cover px-6 xxxs:px-12 lg:px-18 font-roboto pb-26 bg-no-repeat" style={{backgroundImage: `url(${homeImage})`}}>
       <Navbar brandColor="bg-gradient-to-b from-[#FF9238] from-20% to-white bg-clip-text text-transparent"/>
       <div className="1600b:mx-20 2000b:mx-40">
         <div className="py-10 md:py-16 lg:py-24 flex justify-between">
