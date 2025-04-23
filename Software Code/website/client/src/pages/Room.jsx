f7b4a07ac8613cfebafc24b40c829e92623fb523
adarsh <adarsh.goyal@revfin.in>
2025-04-23 21:20:28 +0530

    responisve

--- Software Code/website/client/src/pages/Room.jsx
+++ Software Code/website/client/src/pages/Room.jsx
@@ -29,7 +29,7 @@ export default function Room() {
             className="h-15 cursor-pointer"
             onClick={() => navigate(-1)}
           />
-          <h1 className="text-white font-poppins text-4xl max-xxxs:text-center">
+          <h1 className="text-white font-poppins text-2xl 500b:text-4xl max-xxxs:text-center">
             {roomData.roomName}
           </h1>
           <div className="self-center">
